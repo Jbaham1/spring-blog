b6b5e817993d942e5ddcbfa6902883f482aee101
Jeremiah Baham <baham.jeremiah20@gmail.com>
2021-04-04 12:40:18 -0500

    edit, create merge

--- src/main/java/com/codeup/springblog/controllers/RollDiceController.java
+++ src/main/java/com/codeup/springblog/controllers/RollDiceController.java
@@ -9,12 +9,17 @@ import java.util.concurrent.ThreadLocalRandom;
 
 @Controller
 public class RollDiceController {
+    @GetMapping("roll-dice")
+    public String rollDice(Model viewModel){
+        return "roll-dice";
+    }
+
     @GetMapping("/roll-dice/{guess}")
-    public String rollDice(@PathVariable int guess, Model viewModel ){
+    public String rollDiceGuess(@PathVariable int guess, Model viewModel ){
         int randomNum = ThreadLocalRandom.current().nextInt(1,6+1);
         viewModel.addAttribute("isCorrect", randomNum == guess);
         viewModel.addAttribute("guess", guess);
         viewModel.addAttribute("randomNum", randomNum);
-        return "roll-dice";
+        return "roll-dice-guess";
     }
 }

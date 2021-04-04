33a822dbea614fb83145b9235069469421c40b6a
Jeremiah Baham <baham.jeremiah20@gmail.com>
2021-04-03 20:51:07 -0500

    feat:roll-dice, home, fragments and templates

---
+++ src/main/java/com/codeup/springblog/controllers/RollDiceController.java
@@ -0,0 +1,20 @@
+package com.codeup.springblog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+@Controller
+public class RollDiceController {
+    @GetMapping("/roll-dice/{guess}")
+    public String rollDice(@PathVariable int guess, Model viewModel ){
+        int randomNum = ThreadLocalRandom.current().nextInt(1,6+1);
+        viewModel.addAttribute("isCorrect", randomNum == guess);
+        viewModel.addAttribute("guess", guess);
+        viewModel.addAttribute("randomNum", randomNum);
+        return "roll-dice";
+    }
+}

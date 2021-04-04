6e8de8d3af850c8a7e298f2f7a4c211fd11fdd54
Jeremiah Baham <baham.jeremiah20@gmail.com>
2021-04-03 19:43:10 -0500

    feat: post controller

---
+++ src/main/java/com/codeup/springblog/PostController.java
@@ -0,0 +1,34 @@
+package com.codeup.springblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class PostController {
+    @GetMapping("/posts")
+    @ResponseBody
+    public String allPosts() {
+        return "This is index";
+    }
+
+    @GetMapping("/posts/{id}")
+    @ResponseBody
+    public String singlePost(@PathVariable Long id) {
+        return "This is a single post";
+    }
+    @GetMapping("/posts/create")
+    @ResponseBody
+    public String createPost(){
+        return "This is the creation";
+    }
+    @PostMapping("/posts/create")
+    @ResponseBody
+    public String CreatePost(){
+        return "You created a post";
+    }
+
+}
+

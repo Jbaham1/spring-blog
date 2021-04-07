c9b08eb1d4065f807d88510dce3398c0ef35ba96
Jeremiah Baham <baham.jeremiah20@gmail.com>
2021-04-06 22:22:41 -0500

    post updates

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -6,6 +6,7 @@ import com.codeup.springblog.repo.PostRepo;
 import com.codeup.springblog.repo.UserRepo;
 import com.codeup.springblog.services.EmailService;
 import com.codeup.springblog.services.UserDetailsLoader;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,75 +18,71 @@ import java.util.List;
 
 @Controller
 public class PostController {
-    //    List<Post> posts = new ArrayList<>();
+    @Autowired
+    private EmailService emailService;
+
     private final PostRepo postDao;
     private final UserRepo userDao;
-    private final EmailService emailService;
 
-    public PostController(PostRepo postDao, UserRepo userDao, EmailService emailService) {
+    PostController(PostRepo postDao, UserRepo userDao){
         this.postDao = postDao;
         this.userDao = userDao;
-        this.emailService = emailService;
     }
+
+
     @GetMapping("/posts")
-    public String allPosts(Model viewModel) {
-        List<Post> postFromDB = postDao.findAll();
-        viewModel.addAttribute("posts", postFromDB);
+    public String seeAllPosts(Model viewModel){
+        List<Post> postsFromDb = postDao.findAll();
+        viewModel.addAttribute("posts", postsFromDb);
+        // do not use a / to reference a template
         return "posts/index";
     }
-
     @GetMapping("/posts/{id}")
-    public String singlePost(@PathVariable Long id, Model viewModel) {
-        viewModel.addAttribute("post", postDao.getOne(id));
-        viewModel.addAttribute("user", userDao.getOne(id));
-        return "posts/show";
+    public String showOnePost(@PathVariable Long id, Model vModel){
+        vModel.addAttribute("post", postDao.getOne(id));
+        return "post/show";
     }
-
     @GetMapping("/posts/create")
-    public String viewPostForm(Model vModel){
-        vModel.addAttribute("post",new Post());
+    public String viewPostForm(Model model){
+        model.addAttribute("post", new Post());
         return "posts/create";
     }
-
     @PostMapping("/posts/create")
-    public String createPost(@ModelAttribute Post postToSave){
-
+    public String createPost(@ModelAttribute Post postToBeSaved){
         User userToAdd = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-
         // set the user
-        postToSave.setOwner(userToAdd);
-
+        postToBeSaved.setOwner(userToAdd);
         // Now lets save our post;
-        postDao.save(postToSave);
-
+        postDao.save(postToBeSaved);
+        emailService.prepareAndSend(postToBeSaved, "New Post!", "A new Post has been created in the app");
         return "redirect:/posts";
     }
-
     @GetMapping("/posts/{id}/update")
     public String updatePostForm(@PathVariable Long id, Model model){
-
         Post postFromDb = postDao.getOne(id);
-
         model.addAttribute("oldPost",postFromDb);
-
         return "posts/update";
     }
 
     @PostMapping("/posts/{id}/update")
-    @ResponseBody
-    public String updatePost(@PathVariable Long id,@RequestParam("post_title") String title, @RequestParam("post_body") String body){
+    public String updatePost(@PathVariable Long id, @ModelAttribute Post postToUpdate){
 
-        Post postToSave = new Post(id,title,body);
+        User userToAdd = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        postToUpdate.setId(id);
 
-        postDao.save(postToSave);
-        return "You updated an post.";
+        // set the user
+        postToUpdate.setOwner(userToAdd);
+
+        postDao.save(postToUpdate);
+
+        return "redirect:/posts";
     }
 
     @PostMapping("/posts/{id}/delete")
+    @ResponseBody
     public String deletePost(@PathVariable Long id){
         postDao.deleteById(id);
-        return "posts/index";
-
+        return "You deleted an post.";
     }
 }
 
